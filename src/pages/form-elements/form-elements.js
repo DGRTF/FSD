fd213654a437ead3aed670599ea844ab1579f869
Your Name <futunplay@gmail.com>
2020-03-23 06:07:30 +0300

    add readme

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -3,5 +3,6 @@ import '../../blockUI/check-list/check-list.js';
 import '../../blockUI/dropdown/dropdown.js';
 import { SliderInit } from '../../blockUI/slider/slider.js';
 import "../../blockUI/like-button/like-button.js";
+import "./../../blockUI/comfort/comfort.js";
 
 new SliderInit({});
--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -3,5 +3,6 @@ import "./../../blockUI/calendar/calendar";
 import "./../../blockUI/dropdown/dropdown";
 import {SliderInit} from "./../../blockUI/slider/slider";
 import "./../../blockUI/comfort/comfort.js";
+import "./../../blockUI/present-room/present-room";
 
 new SliderInit({});
