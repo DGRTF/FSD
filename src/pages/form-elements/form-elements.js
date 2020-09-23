30bcd1a7b25ae035778a6009198f46f24bb145b1
Your Name <futunplay@gmail.com>
2020-09-23 13:38:06 +0300

    add quicksand

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -3,5 +3,6 @@ import '../../blockUI/dropdown/dropdown.js';
 import { SliderInit } from '../../blockUI/slider/slider.js';
 import "../../blockUI/like-button/like-button.js";
 import "./../../blockUI/comfort/comfort.js";
+import './../../blockUI/calendar/calendar.js';
 
 new SliderInit({});
