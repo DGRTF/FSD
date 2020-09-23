cb80a2ce415b3b7f916fa73bca38c43b1eddc939
Your Name <futunplay@gmail.com>
2020-09-23 11:46:24 +0300

    В card виден только календарь

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -3,19 +3,3 @@ import './../../blockUI/calendar/calendar.js';
 import "./../../blockUI/search-room/search-room.js";
 import "./../../blockUI/calculate-price/calculate-price.js";
 import "./../../blockUI/present-room/present-room.js";
-
-// function calenr() {
-//     let calendar1 = $('.js-calendar__plugin').datepicker({
-//         minDate: new Date(),
-//         clearButton: true,
-//         // prevHtml: "<div>arrow_back</div>",
-//         // nextHtml: "<div>arrow_forward</div>",
-//         onRenderCell: function (date, cellType) {
-//             return {
-//                 html: "<a>" + date.getDate() + "</a>"
-//             }
-//         },
-//     })
-// }
-
-// calenr();
