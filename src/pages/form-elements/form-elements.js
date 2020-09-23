b4490bb0ed55e638d94f8144c20c7323d2d00bbc
Your Name <futunplay@gmail.com>
2020-09-23 10:50:38 +0300

    убран лишний js

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,5 +1,4 @@
 import './form-elements.scss';
-import '../../blockUI/check-list/check-list.js';
 import '../../blockUI/dropdown/dropdown.js';
 import { SliderInit } from '../../blockUI/slider/slider.js';
 import "../../blockUI/like-button/like-button.js";
