0e41a3e3ae2838753660dc0a96e8fb17a1199206
Your Name <futunplay@gmail.com>
2020-10-06 22:42:16 +0300

    add image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -73,12 +73,39 @@ module.exports =
 
                 {
                     test: /\.(jpeg|jpg|png|gif)$/,
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'img',
-                        publicPath: "./../img"
-                    }
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'img',
+                                publicPath: "./../img"
+                            }
+                        },
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                mozjpeg: {
+                                    progressive: true,
+                                },
+                                // optipng.enabled: false will disable optipng
+                                optipng: {
+                                    enabled: false,
+                                },
+                                pngquant: {
+                                    quality: [0.65, 0.90],
+                                    speed: 4
+                                },
+                                gifsicle: {
+                                    interlaced: false,
+                                },
+                                // the webp option will enable WEBP
+                                webp: {
+                                    quality: 75
+                                }
+                            }
+                        },
+                    ],
                 },
 
                 {
