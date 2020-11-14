de9ef95fe025179f180ee738e83519867dd6850f
Your Name <futunplay@gmail.com>
2020-11-14 21:00:10 +0300

    fix

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -6,3 +6,14 @@ import "./../../blockUI/comfort/comfort.js";
 import './../../blockUI/calendar/calendar.js';
 
 new SliderInit({});
+
+(function buttonDisable(){
+  const containerButton = document.querySelector('.form-elements__div-button-d');
+  const button = containerButton.querySelector('.button');
+  button.disabled = true;
+
+  const containerButtonBorder = document.querySelector('.form-elements__div-button-border-d');
+  const buttonBorder = containerButtonBorder.querySelector('.button-border');
+  buttonBorder.disabled = true;
+})
+  ();
