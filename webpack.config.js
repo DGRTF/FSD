42f8bba35723adefe87796648134e14a200cc69d
Your Name <futunplay@gmail.com>
2021-02-14 22:03:34 +0500

    #18.3

--- webpack.config.js
+++ webpack.config.js
@@ -14,159 +14,159 @@ const searchEntry = new SearchEntry(pagesPath);
 let rootDirectory = '';
 
 module.exports = (env, options) => {
-    if (options.mode === 'production')
-        rootDirectory = '/FSD';
+  if (options.mode === 'production')
+    rootDirectory = '/FSD';
 
-    return {
-        entry: merge([searchEntry.entry, { index: './src/index.js' }]),
-        output:
-        {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'scripts/[name].js',
-        },
-        plugins: searchEntry.HWPluginObjectArr.concat([
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                "window.jQuery": "jquery",
-            }),
+  return {
+    entry: merge([searchEntry.entry, {  }]),
+    output:
+    {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'scripts/[name].js',
+    },
+    plugins: searchEntry.HWPluginObjectArr.concat([
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        "window.jQuery": "jquery",
+      }),
 
-            new HtmlWebpackPlugin({
-                inject: true,
-                chunks: ['index'],
-                filename: 'index.html',
-                template: './src/index.pug',
-                favicon: './src/img/favicon.ico',
-            }),
+      new HtmlWebpackPlugin({
+        inject: true,
+        chunks: ['index'],
+        filename: 'index.html',
+        template: './src/index.pug',
+        favicon: './src/img/favicon.ico',
+      }),
 
-            new FaviconsWebpackPlugin({
-                publicPath: './',
-                logo: './src/img/label.svg',
-            }),
+      new FaviconsWebpackPlugin({
+        publicPath: './',
+        logo: './src/img/label.svg',
+      }),
 
-            new MiniCssExtractPlugin({
-                filename: "css/[name].css",
-            }),
+      new MiniCssExtractPlugin({
+        filename: "css/[name].css",
+      }),
 
-        ]),
-        module:
-        {
-            rules:
-                [
-                    {
-                        test: /\.m?js$/,
-                        exclude: /(node_modules|bower_components)/,
-                        use: {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: [
-                                    '@babel/preset-env'
-                                ]
-                            }
-                        }
-                    },
+    ]),
+    module:
+    {
+      rules:
+        [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env'
+                ]
+              }
+            }
+          },
 
-                    {
-                        test: /\.css$/,
-                        use: [
-                            'style-loader',
-                            {
-                                loader: MiniCssExtractPlugin.loader,
-                            },
-                            'css-loader',
-                            'postcss-loader'
-                        ]
-                    },
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: MiniCssExtractPlugin.loader,
+              },
+              'css-loader',
+              'postcss-loader'
+            ]
+          },
 
-                    {
-                        test: /\.(jpeg|jpg|png|gif)$/,
-                        use: [
-                            {
-                                loader: 'file-loader',
-                                options: {
-                                    name: '[name].[ext]',
-                                    outputPath: 'img',
-                                    publicPath: rootDirectory + "/img"
-                                }
-                            },
-                            {
-                                loader: 'image-webpack-loader',
-                                options: {
-                                    mozjpeg: {
-                                        progressive: true,
-                                    },
-                                    // optipng.enabled: false will disable optipng
-                                    optipng: {
-                                        enabled: false,
-                                    },
-                                    pngquant: {
-                                        quality: [0.65, 0.90],
-                                        speed: 4
-                                    },
-                                    gifsicle: {
-                                        interlaced: false,
-                                    },
-                                    // the webp option will enable WEBP
-                                    webp: {
-                                        quality: 75
-                                    }
-                                }
-                            },
-                        ],
-                    },
+          {
+            test: /\.(jpeg|jpg|png|gif)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'img',
+                  publicPath: rootDirectory + "/img"
+                }
+              },
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  mozjpeg: {
+                    progressive: true,
+                  },
+                  // optipng.enabled: false will disable optipng
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: [0.65, 0.90],
+                    speed: 4
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                  // the webp option will enable WEBP
+                  webp: {
+                    quality: 75
+                  }
+                }
+              },
+            ],
+          },
 
-                    {
-                        test: /\.(ttf|woff|woff2|eot)$/,
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts',
-                            publicPath: rootDirectory + "/fonts"
-                        }
-                    },
+          {
+            test: /\.(ttf|woff|woff2|eot)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts',
+              publicPath: rootDirectory + "/fonts"
+            }
+          },
 
-                    {
-                        test: /\.svg$/,
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'img',
-                            publicPath: rootDirectory + "/img"
-                        }
-                    },
+          {
+            test: /\.svg$/,
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img',
+              publicPath: rootDirectory + "/img"
+            }
+          },
 
-                    {
-                        test: /\.scss$/,
-                        use: [
-                            // Creates `style` nodes from JS strings
-                            { loader: 'style-loader' },
-                            {
-                                loader: MiniCssExtractPlugin.loader,
-                            },
-                            // Translates CSS into CommonJS
-                            { loader: 'css-loader' },
-                            { loader: 'postcss-loader' },
-                            { loader: 'resolve-url-loader' },
-                            {
-                                // Compiles Sass to CSS
-                                loader: 'sass-loader',
-                                options: {
-                                    sourceMap: true
-                                }
-                            }
-                        ]
-                    },
+          {
+            test: /\.scss$/,
+            use: [
+              // Creates `style` nodes from JS strings
+              { loader: 'style-loader' },
+              {
+                loader: MiniCssExtractPlugin.loader,
+              },
+              // Translates CSS into CommonJS
+              { loader: 'css-loader' },
+              { loader: 'postcss-loader' },
+              { loader: 'resolve-url-loader' },
+              {
+                // Compiles Sass to CSS
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          },
 
-                    {
-                        test: /\.pug$/,
-                        loader: 'pug-loader',
-                        options:
-                        {
-                            pretty: true
-                        }
-                    }
+          {
+            test: /\.pug$/,
+            loader: 'pug-loader',
+            options:
+            {
+              pretty: true
+            }
+          }
 
-                ],
-        },
-    };
+        ],
+    },
+  };
 }
